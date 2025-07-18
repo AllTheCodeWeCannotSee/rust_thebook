6ee2424b702a42618ab72beaf3f9e022c7c84ebd
AllTheCodeWeCannotSee <183735517@qq.com>
2025-07-18 11:28:32 +0800

    4. 认识所有权

---
+++ ownership/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s = String::from("hello world");
+    let first_word = first_word(&s);
+    println!("{first_word}");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
